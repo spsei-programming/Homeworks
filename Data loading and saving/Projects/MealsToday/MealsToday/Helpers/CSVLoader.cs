5a055ca86ceee51caa8c713b9d38de50b4ba8515
Ondrej Valenta <me@ondrejvalenta.com>
2016-11-28 15:00:15 +0100

    MealsTodayV2

---
+++ Data loading and saving/Projects/MealsToday/MealsToday/Helpers/CSVLoader.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Runtime.CompilerServices;
+using MealsToday.Data;
+
+namespace MealsToday.Helpers
+{
+	public static class CSVLoader
+	{
+		private static User parseUserRow(string row)
+		{
+			var user = new User();
+
+			string[] dataParts = row.Split(';');
+
+			user.UserId = Convert.ToInt32(dataParts[0]);
+			user.Username = dataParts[1];
+			user.FirstName = dataParts[2];
+			user.LastName = dataParts[3];
+
+#warning Finish user role loading and parsing
+			//user.Roles
+
+			return user;
+		}
+
+		private static Allergen parseAllergenRow(string row)
+		{
+			return new Allergen();
+		}
+
+		private static Meal parseMealRow(string row)
+		{
+			return new Meal();
+		}
+
+		public static List<User> LoadUserData(string filePath)
+		{
+			if (!File.Exists(filePath))
+			{
+				throw new FileNotFoundException("File not found", filePath);
+			}
+
+			string[] allLines = File.ReadAllLines(filePath);
+
+			for (int i = 1; i < allLines.Length; i++)
+			{
+				var user = parseUserRow(allLines[i]);
+			}
+
+			return new List<User>();
+		}
+
+		public static List<Allergen> LoadAllergenData(string filePath)
+		{
+			return new List<Allergen>();
+		}
+
+		public static List<Meal> LoadMealData(string filePath)
+		{
+			return new List<Meal>();
+		}
+	}
+
+}
---
+++ Data loading and saving/Projects/MealsToday/MealsToday/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MealsToday.Helpers;
+
+namespace MealsToday
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			CSVLoader.LoadUserData(@"C:\Git\spsei-programming\Homeworks\Data loading and saving\CSV files\Users.csv");
+		}
+	}
+}
